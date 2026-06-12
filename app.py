a218632881b0e6ba4d1d18e50362aecb67b00fcf
Seoyoung <108845232+syee22@users.noreply.github.com>
2026-06-12 21:21:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import io
 import re
-
+MONTH_NAMES = {1:'Jan', 2:'Feb', 3:'Mar', 4:'Apr', 5:'May', 6:'Jun', 7:'Jul', 8:'Aug', 9:'Sep', 10:'Oct', 11:'Nov', 12:'Dec'}
 # ==========================================
 # 1. 페이지 설정 및 전역 CSS 주입
 # ==========================================
