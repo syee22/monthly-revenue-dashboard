750f8759393074bc866bc76f3ac584b55dd50d0c
Seoyoung <108845232+syee22@users.noreply.github.com>
2026-06-12 20:42:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -442,7 +442,7 @@ if uploaded_file:
         
         trend_df.loc['TTL (K.€)'] = trend_df.sum(numeric_only=True)
         trend_df.index.name = ''
-        trend_df.columns = [' '.join(col).strip() for col in trend_df.columns.values]
+        
         return trend_df
 
     # ==========================================
