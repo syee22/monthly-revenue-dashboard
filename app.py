b1f81672d545e56479fdf916ea28a2d1234e2e4d
Seoyoung <108845232+syee22@users.noreply.github.com>
2026-06-14 21:58:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -50,15 +50,15 @@ h3 { font-size: 1.1rem !important; margin-top: 1rem !important; margin-bottom: 0
 
 /* 데이터가 담긴 행(tbody) 높이 12px 강제 고정 */
 .report-table tbody tr { 
-    height: 14px !important; 
-    max-height: 14px !important;
+    height: 15px !important; 
+    max-height: 15px !important;
 }
 .report-table tbody td, .report-table tbody th { 
-    height: 14px !important; 
-    max-height: 14px !important;
+    height: 15px !important; 
+    max-height: 15px !important;
     padding-top: 0px !important; 
     padding-bottom: 0px !important; 
-    line-height: 14px !important; 
+    line-height: 15px !important; 
 }
 </style>""", unsafe_allow_html=True)
 
