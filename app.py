e9bb1e9d86f1ef719c305dce824487955f89dc58
Seoyoung <108845232+syee22@users.noreply.github.com>
2026-06-23 22:59:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -379,30 +379,22 @@ if selected_menu == "매출 보고서":
         def get_biz_report(df, biz_type, year, month):
             prev_year, prev_month = (year - 1, 12) if month == 1 else (year, month - 1)
             
-            # 데이터 필터링
+            # 1. 데이터 필터링
             df_biz = df[(df['Business Type'].str.contains(biz_type, case=False, na=False))].copy()
             
-            # 기간별 정의
+            # 2. 기간별 정의
             m_str, pm_str = MONTH_NAMES.get(month, f'{month}'), MONTH_NAMES.get(prev_month, f'{prev_month}')
-            p_curr = f'{m_str}. {year}'
-            p_ytd = f'YTD {m_str}. {year}'
-            p_ttl = f'{year} TTL'
-            p_prev = f'{pm_str}. {prev_year}'
+            p_curr, p_ytd, p_ttl, p_prev = f'{m_str}. {year}', f'YTD {m_str}. {year}', f'{year} TTL', f'{pm_str}. {prev_year}'
             
             brands = ['HYU', 'KIA'] if biz_type == 'Power' else ['HYU', 'KIA', 'GM']
             results = []
             
             for brand in brands:
                 # 해당 브랜드 데이터 추출
-                if brand == 'GM':
-                    b_data = df_biz[(df_biz['Project'] == 'GM')]
-                else:
-                    b_data = df_biz[(df_biz['Group 2'] == brand)]
-                    
+                b_data = df_biz[(df_biz['Project'] == 'GM')] if brand == 'GM' else df_biz[(df_biz['Group 2'] == brand)]
                 if b_data.empty: continue
                 
-                # 1. 통합 데이터프레임 구성 (MultiIndex: Project, Con, SOP)
-                # 모든 기간의 데이터를 한 번에 피벗
+                # 통합 피벗 함수
                 def get_rev(d, year_f, month_f=None, is_ytd=False):
                     mask = (d['Year'] == year_f)
                     if month_f: 
@@ -415,30 +407,28 @@ if selected_menu == "매출 보고서":
                 data_y = get_rev(b_data, year, month, is_ytd=True)
                 data_fy = get_rev(b_data, year)
                 
-                # 통합 인덱스 (모든 프로젝트)
+                # 모든 기간의 프로젝트를 포함하는 통합 인덱스 생성
                 all_indices = data_prev.index.union(data_m.index).union(data_y.index).union(data_fy.index)
-                
-                # 데이터프레임 재구성
                 df_all = pd.DataFrame(index=all_indices)
                 
-                # 칼럼 생성: (기간, 분류)
+                # 데이터 병합 및 ACHI % 계산
                 def add_cols(target_df, period, source_df):
                     for col in ['26 FC1', 'ACT']:
                         if col in source_df.columns: target_df[(period, col)] = source_df[col]
-                    # ACHI 계산
                     if (period, 'ACT') in target_df.columns and (period, '26 FC1') in target_df.columns:
                         target_df[(period, 'ACHI %')] = target_df[(period, 'ACT')] / target_df[(period, '26 FC1')].replace(0, np.nan)
                 
-                add_cols(df_all, p_prev, data_prev) # prev는 ACT만 사용
+                add_cols(df_all, p_prev, data_prev)
                 add_cols(df_all, p_curr, data_m)
                 add_cols(df_all, p_ytd, data_y)
                 add_cols(df_all, p_ttl, data_fy)
-                
                 df_all = df_all.fillna(0)
                 
-                # 2. Top 5 선정 (기준: 월간 ACT)
+                # 3. Top 5 선정 및 Others 처리 (기준: 월간 ACT)
                 top_n = 5
-                top_indices = df_all.sort_values(by=(p_curr, 'ACT'), ascending=False).head(top_n).index
+                # 0이 아닌 것들 중 상위 5개
+                active_projects = df_all[df_all[(p_curr, 'ACT')] > 0]
+                top_indices = active_projects.sort_values(by=(p_curr, 'ACT'), ascending=False).head(top_n).index
                 
                 top_df = df_all.loc[df_all.index.isin(top_indices)].sort_values(by=(p_curr, 'ACT'), ascending=False)
                 others_df = df_all.loc[~df_all.index.isin(top_indices)].sum().to_frame().T
@@ -453,20 +443,27 @@ if selected_menu == "매출 보고서":
                         subtotal[(p, 'ACHI %')] = subtotal[(p, 'ACT')] / subtotal[(p, '26 FC1')] if subtotal[(p, '26 FC1')] != 0 else 0
                 
                 final_brand_df = pd.concat([final_brand_df, subtotal.to_frame().T])
-                final_brand_df.index = pd.MultiIndex.from_tuples(
-                    [(brand, idx[0], idx[1], idx[2]) if idx[0] != 0 else (brand, f'{brand}_소계', '', '') for idx in final_brand_df.index],
-                    names=['Cust. GR', 'Project', 'Con.', 'SOP']
-                )
                 
+                # 4. 안전한 인덱스 재구성
+                new_index = []
+                # 마지막 행은 소계임이 보장됨
+                for i, idx in enumerate(final_brand_df.index):
+                    if i == len(final_brand_df) - 1:
+                        new_index.append((brand, f'{brand}_소계', '', ''))
+                    elif idx[0] == 'Others':
+                        new_index.append((brand, 'Others', '', ''))
+                    else:
+                        new_index.append((brand, idx[0], idx[1], idx[2]))
+                        
+                final_brand_df.index = pd.MultiIndex.from_tuples(new_index, names=['Cust. GR', 'Project', 'Con.', 'SOP'])
                 results.append(final_brand_df)
         
             if not results: return pd.DataFrame(), [p_curr, p_ytd, p_ttl]
             
-            # 최종 병합 및 Grand Total
+            # 5. 최종 병합 및 Grand Total 계산
             final_df = pd.concat(results)
             grand_total = final_df[final_df.index.get_level_values(1).str.contains('_소계', na=False)].sum()
             
-            # Grand Total ACHI 재계산
             for p in [p_curr, p_ytd, p_ttl]:
                 if (p, '26 FC1') in grand_total.index:
                     grand_total[(p, 'ACHI %')] = grand_total[(p, 'ACT')] / grand_total[(p, '26 FC1')] if grand_total[(p, '26 FC1')] != 0 else 0
